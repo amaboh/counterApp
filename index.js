b5c0f924f2ceb8694290977d6d3bb737a91ed5a9
amaboh <amabocanon@gmail.com>
2022-01-15 23:56:01 +0100

    Edit style.css and index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
 let count = 0;
 let countEl = document.getElementById("count-el")
+let saveBtn = document.getElementById("save-btn")
+let saveEl = document.getElementById("save-el")
 
 function increment(){
     count+=1
     countEl.innerHTML = count
+}
+
+function save(){
+    let saveStr = count + " - "
+    saveEl.innerText += saveStr
+    console.log(count)
 }
